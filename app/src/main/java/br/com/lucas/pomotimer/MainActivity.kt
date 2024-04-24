6c81dddf4256c61420d04882fce66c126e74c907
Lucas Moro <lucas.rmoro1@gmail.com>
2024-04-24 18:42:25 -0300

    Adding View Binding

--- app/src/main/java/br/com/lucas/pomotimer/MainActivity.kt
+++ app/src/main/java/br/com/lucas/pomotimer/MainActivity.kt
@@ -1,11 +1,15 @@
 package br.com.lucas.pomotimer
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import br.com.lucas.pomotimer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 }
